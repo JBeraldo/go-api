2ee0fe9ba7289ab7988150c7881fbc06c97e0a66
JBeraldo <joaoangelo409@gmail.com>
2024-07-09 20:08:54 -0300

    Feat: testing endpoints

--- service/user/route.go
+++ service/user/route.go
@@ -7,6 +7,7 @@ import (
 	"github.com/JBeraldo/go-api/service/auth"
 	"github.com/JBeraldo/go-api/types"
 	"github.com/JBeraldo/go-api/utils"
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 )
 
@@ -30,8 +31,15 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	var payload types.RegisterUserPayload
-	if err := utils.ParseJSON(r, payload); err != nil {
+	if err := utils.ParseJSON(r, &payload); err != nil {
 		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
+		return
 	}
 
 	_, err := h.store.GetUserByEmail(payload.Email)
