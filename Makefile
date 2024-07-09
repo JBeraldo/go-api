b895c39451410ec255c6119e3292d9f51d439a21
JBeraldo <joaoangelo409@gmail.com>
2024-07-08 22:07:33 -0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,6 @@
+build:
+	@go build -o bin/go-api cmd/main.go
+test:
+	@go test -v ./...
+run: build
+	@./bin/go-api
---
+++ service/user/route.go
@@ -0,0 +1,62 @@
+package user
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/JBeraldo/go-api/service/auth"
+	"github.com/JBeraldo/go-api/types"
+	"github.com/JBeraldo/go-api/utils"
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+	store types.UserStore
+}
+
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/login", h.handleLogin).Methods("GET")
+	router.HandleFunc("/register", h.handleRegister).Methods("POST")
+
+}
+
+func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ping")
+}
+
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
+}
